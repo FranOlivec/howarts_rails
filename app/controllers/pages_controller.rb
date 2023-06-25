103280d556e0fa1b4ff119bb8454c54836ba0a93
Francisca Olivares <francisca.olivares.vecchiola@gmail.com>
2023-06-25 01:21:27 -0400

    Aplicación de CSS y creacion de vistas adicionales

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,13 @@ class PagesController < ApplicationController
   def index
     @characters = Character.all
   end
+
+  def hogwarts
+    render 'hogwarts'
+  end
+
+  def ilvermorny
+    render 'ilvermorny'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/index'
+  get '/hogwarts', to: 'pages#hogwarts'
+  get '/ilvermorny', to: 'pages#ilvermorny'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
